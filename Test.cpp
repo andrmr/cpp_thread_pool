0a74befee8e5c69d60a8951ea41b74ed2b021973
Andrei Murariu <andrei.murariu@continental-corporation.com>
2018-10-04 15:45:43 +0300

    Cleanup tests.

--- Test.cpp
+++ Test.cpp
@@ -1,13 +1,14 @@
 #include "ThreadPool.h"
 #include <iostream>
 
-int func1(int x)
+template <typename T>
+T func1(T x)
 {
     std::cout << __PRETTY_FUNCTION__ << " on thread: " << std::this_thread::get_id() << "\n";
     return x;
 }
 
-int func2(uint32_t x,  uint16_t y, uint8_t z)
+uint32_t func2(uint32_t x, uint16_t y, uint8_t z)
 {
     std::cout << __PRETTY_FUNCTION__ << " on thread: " << std::this_thread::get_id() << "\n";
     return (x + y + z);
@@ -17,13 +18,13 @@ int main()
 {
     TP::ThreadPool tp;
 
-
-    auto f1 = tp.addTask(func1, 9);
+    auto f1 = tp.addTask(func1<uint32_t>, 9u);
 
     uint16_t y = 5;
     auto f2 = tp.addTask(func2, 10, y, 5);
 
     f1.get();
     f2.get();
+
     return 0;
 }
